3a5e0b35f6787a39f70409fdbceb63ec5e705872
niedlipatrik <niedlipatrik@gmail.com>
2019-09-11 10:19:15 +0200

    actualizacion eslint-utils

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,30 +1,42 @@
-import React, { Component } from "react";
-import api from "../../services/api";
+    import React, { Component } from "react";
+    import api from "../../services/api";
+    import './styles.css'
 
-export default class Main extends Component{
-     // Para criar variaveis reativas, deve-se declara-las no méthodo state através de um listening pelo methodo render()
-    state = {
-        products: [],
+    export default class Main extends Component{
+        // Para criar variaveis reativas, deve-se declara-las no méthodo state através de um listening pelo methodo render()
+        state = {
+            products: [],
+        };
+
+
+        componentDidMount(){
+            this.loadProducts();
+        }
+
+    loadProducts = async () => {
+        const response = await api.get("/products");
+
+        this.setState({ products: response.data.docs });
     };
 
+    render() {
+        const { products } = this.state;
+        return (
+        <div className="product-list">
+        {products.map(products => (
+                <article key={products._id}>
+                <strong>{products.title}</strong>
+                <p>{products.description}</p>
 
-    componentDidMount(){
-        this.loadProducts();
-    }
+                <a href="">Acedar</a>
+                </article>
+            ))}
+                    <div className="actions">
+            <button onClick={this.prevPage}>Anterior</button>
+            <button onClick={this.nextPage}>Próximo</button>
 
-loadProducts = async () => {
-    const response = await api.get("/products");
-
-    this.setState({ products: response.data.docs });
-  };
-
-  render() {
-    return (
-    <div className="product-list">
-    {this.state.products.map(products => (
-            <h2>{products.title}</h2>
-        ))}
-    </div>
-    );
-  };
-}
+            </div>
+        </div>
+        );
+    };
+    };
